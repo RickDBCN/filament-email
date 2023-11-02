a3ffe32fa1532e3edd97d331ef8d5f576f77ff5d
mstfkhazaal <47450090+mstfkhazaal@users.noreply.github.com>
2023-11-02 12:10:18 +0200

    Update EmailResource.php

--- src/Filament/Resources/EmailResource.php
+++ src/Filament/Resources/EmailResource.php
@@ -39,7 +39,7 @@ class EmailResource extends Resource
             }else{
              return  __($str);
             }
-        }catch($ex){
+        }catch(\Exception  $ex){
             return  __('Email log');
         }
     }
@@ -53,7 +53,7 @@ class EmailResource extends Resource
             }else{
              return  __($str);
             }
-        }catch($ex){
+        }catch(\Exception  $ex){
             return  parent::getNavigationGroup();
         }
     }
