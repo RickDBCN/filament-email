b4b3f649a16d293b65ee0ee37a31ed7b38795dc4
Tieme Alberts <52133032+sitenzo@users.noreply.github.com>
2023-12-16 00:02:59 +0100

    Update filament-email.php

--- config/filament-email.php
+++ config/filament-email.php
@@ -7,12 +7,10 @@ return [
 
     'resource' => [
         'class' => EmailResource::class,
-        'group' => null,
         'sort' => null,
         'default_sort_column' => 'created_at',
         'default_sort_direction' => 'desc',
     ],
 
-    'keep_email_for_days' => 60,
-    'label' => null,
+    'keep_email_for_days' => 60
 ];
