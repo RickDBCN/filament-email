37f5dfa3423c5bd6aa2233cfed322b16ebdf14bd
Tieme Alberts <52133032+sitenzo@users.noreply.github.com>
2023-12-16 00:42:13 +0100

    Update email-log.php

--- resources/lang/nl/email-log.php
+++ resources/lang/nl/email-log.php
@@ -1,6 +1,28 @@
 <?php 
 
+<?php 
+
 return [
-  'group' => null,
-  'label' => 'E-maillogboek',
+    // Page
+    'group' => null,
+    'label' => 'E-maillogboek',
+    
+    // Form
+    'created_at' => 'Aangemaakt op',
+    'cc' => 'CC',
+    'bcc' => 'BCC',
+
+    // Table 
+    'preview' => 'Voorbeeld',
+    'send_again' => 'Opnieuw versturen',
+    'created_at' => 'Datum en tijd verzonden',
+
+    // Form and Table
+    'from' => 'Van',
+    'to' => 'Naar',
+    'subject' => 'Onderwerp',
+    
+    // notifications
+    'e-mail-has-been-successfully-sent' => 'E-mail is succesvol verzonden',
+    'something-went-wrong' => 'Er is iets fout gegaan'
 ];
