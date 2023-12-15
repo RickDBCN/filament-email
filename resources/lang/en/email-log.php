ea2dee610733b648ad25295e34e97383defb81b8
Tieme Alberts <52133032+sitenzo@users.noreply.github.com>
2023-12-16 00:40:24 +0100

    Update email-log.php

--- resources/lang/en/email-log.php
+++ resources/lang/en/email-log.php
@@ -1,6 +1,26 @@
 <?php 
 
 return [
-  'group' => null,
-  'label' => 'E-Mail Log',
+    // Page
+    'group' => null,
+    'label' => 'E-Mail Log',
+    
+    // Form
+    'created_at' => 'Created at',
+    'cc' => 'CC',
+    'bcc' => 'BCC',
+
+    // Table 
+    'preview' => 'Preview',
+    'send_again' => 'Send Again',
+    'created_at' => 'Date and time sent',
+
+    // Form and Table
+    'from' => 'From',
+    'to' => 'To',
+    'subject' => 'Subject',
+    
+    // notifications
+    'e-mail-has-been-successfully-sent' => 'E-mail has been successfully sent',
+    'something-went-wrong' => 'Something went wrong'
 ];
