e84020d8dbef6356d0542a820c657dcc0dd6a6ba
marcogermani87 <marcogermani87@users.noreply.github.com>
2024-04-17 14:36:08 +0000

    Fix styling

--- tests/Models/User.php
+++ tests/Models/User.php
@@ -7,9 +7,9 @@ use Filament\Panel;
 use Illuminate\Auth\Authenticatable;
 use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
 use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Foundation\Auth\Access\Authorizable;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class User extends Model implements AuthenticatableContract, AuthorizableContract, FilamentUser
 {
