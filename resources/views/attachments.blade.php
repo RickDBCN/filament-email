657f9f6a1ba7d0db09661fd6f0b62fd16b9b34de
Marco Germani <info@marcogermani.it>
2024-05-13 22:25:32 +0200

    Enhanched view attachments layout. Updated screenshot.

--- resources/views/attachments.blade.php
+++ resources/views/attachments.blade.php
@@ -1,21 +1,21 @@
-<div>
-    <?php $attachments = $getRecord()->attachments; ?>
-    @if(!empty($attachments))
-        <ul class="max-w-md divide-y divide-gray-200 dark:divide-gray-700">
-            @foreach($attachments as $attachment)
-                <li class="pb-4 pt-4 sm:pb-4">
-                    <div class="flex items-center space-x-4 rtl:space-x-reverse">
-                        <div class="flex-1 min-w-0">
-                            <p class="text-sm font-medium text-gray-900 truncate dark:text-white">
-                                <strong>{{ $attachment['name'] }}</strong>
-                            </p>
-                        </div>
-                        <div class="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
-                            {{ ($this->downloadAction)(['path' => $attachment['path'], 'name' => $attachment['name'] , 'type' => $attachment['contentType']]) }}
-                        </div>
+<?php $attachments = $getRecord()->attachments; ?>
+
+@if(!empty($attachments))
+    <div class="flex flex-wrap gap-8">
+        @foreach($attachments as $attachment)
+            <div class="basis-1/4 me-3">
+                <div class="flex items-center space-x-6 rtl:space-x-reverse">
+                    <div class="flex-initial min-w-0 pe-4">
+                        <p class="text-sm font-medium text-gray-900 truncate dark:text-white">
+                            <strong>{{ $attachment['name'] }}</strong>
+                        </p>
                     </div>
-                </li>
-            @endforeach
-        </ul>
-    @endif
-</div>
+                    <div class="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
+                        {{ ($this->downloadAction)(['path' => $attachment['path'], 'name' => $attachment['name'] , 'type' => $attachment['contentType']]) }}
+                    </div>
+                </div>
+            </div>
+        @endforeach
+    </div>
+@endif
+
